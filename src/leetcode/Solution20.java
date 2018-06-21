335fbbc969c88b5493e92c7239a6b50b1bd1ef98
DESKTOP-34I988G\Frank <1002039945@qq.com>
2018-06-21 20:27:45 +0800

    项目初始化

---
+++ src/leetcode/Solution20.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author Created By Frank
+ * @date 2018-06-21 20:10
+ */
+
+
+//20. 有效的括号 - LeetCode (中国)
+//https://leetcode-cn.com/problems/valid-parentheses/description/
+
+public class Solution20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '[' || ch == '{' || ch == '(') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+
+                char topch = stack.pop();
+                if (ch == ']' && topch != '[')
+                    return false;
+                if (ch == ')' && topch != '(')
+                    return false;
+                if (ch == '}' && topch != '{')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution20().isValid("[[]]"));
+        System.out.println(new Solution20().isValid("[[]][["));
+    }
+}
