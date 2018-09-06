bfa4e86b7be8d6402e88d5c2874ea543a53c798f
DESKTOP-34I988G\Frank <1002039945@qq.com>
2018-09-06 20:07:44 +0800

    no message

---
+++ src/interview/code_0906_美团/Main3.java
@@ -0,0 +1,17 @@
+package interview.code_0906_美团;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main3 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(new BufferedReader(new FileReader("D:\\gitdoc\\interview_code\\src\\interview\\code_0906_美团\\input3.txt")));
+
+
+    }
+
+}
